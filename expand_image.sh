dbfb238a119d4395c9d7287ede97e047aa652536
Chris J <crattis@users.noreply.github.com>
2015-09-11 21:07:55 -0400

    Update expand_image.sh
    more reading changes, and fixed one last bad command

--- expand_image.sh
+++ expand_image.sh
@@ -12,7 +12,7 @@ apt-get install -y triggerhappy lua5.1
 
 echo -e "\n"
 echo "Downloading raspi config package from Debian repository, 2015-07-06_all version"
-echo -e"\n"
+echo -e "\n"
 sleep 10
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150706_all.deb
 
@@ -24,8 +24,10 @@ dpkg -i raspi-config_20150706_all.deb
 chmod 755 raspi-config
 
 echo -e "\n"
-echo -e "about to start raspi-config. 2 steps needed. First 1) Expand Filesystem. \n Second 3)Enable Boot to Desktop/Scratch chagne to commandline"
-echo -e"\n"
+echo -e "about to start raspi-config. 2 steps needed."
+echo -e "First 1) Expand Filesystem."
+echo -e "Second 3)Enable Boot to Desktop/Scratch chagne to commandline"
+echo -e "\n"
 sleep 10
 
-./raspi-config
+raspi-config
