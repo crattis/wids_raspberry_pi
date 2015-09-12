9fdc30dc53b3e68e65897c21237233b4cc7b082c
Chris J <crattis@users.noreply.github.com>
2015-09-11 20:55:01 -0400

    Update expand_image.sh
    fixed a couple of typos, that looked to break the script and also added sleep statements and blank lines to make reading messages easier

--- expand_image.sh
+++ expand_image.sh
@@ -1,22 +1,31 @@
-!# /bin/bash
+#! /bin/bash
 # commands are expecting you to be logged in as root on raspberry pi running kali.
 #
 # Chris J. rattis@gmail.com / chrisj@rattis.net @rattis twitter crattis on git
 
 # prep apt, and download needed files.
 echo "Downloading and installing triggerhappy and lau5.1 from Kali repositories"
+echo -e "\n"
+sleep 10
 apt-get update
-apt-get install  -y triggerhappy lau5.1 
+apt-get install -y triggerhappy lua5.1 
 
+echo -e "\n"
 echo "Downloading raspi config package from Debian repository, 2015-07-06_all version"
+echo -e"\n"
+sleep 10
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150706_all.deb
 
+echo -e "\n"
 echo "installing raspi-config debian package"
+echo -e "\n"
+sleep 10
 dpkg -i raspi-config_20150706_all.deb
 chmod 755 raspi-config
 
+echo -e "\n"
 echo -e "about to start raspi-config. 2 steps needed. First 1) Expand Filesystem. \n Second 3)Enable Boot to Desktop/Scratch chagne to commandline"
-
+echo -e"\n"
 sleep 10
 
 ./raspi-config
